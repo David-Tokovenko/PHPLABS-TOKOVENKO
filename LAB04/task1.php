7702e31151a705773002969ad60c7233d68469ff
David <kn1b22.tokovenko@kpnu.edu.ua>
2025-04-10 12:33:38 +0300

    .

---
+++ LAB04/task1.php
@@ -0,0 +1,12 @@
+$days = [
+"Monday" => "Понеділок",
+"Tuesday" => "Вівторок",
+"Wednesday" => "Середа",
+"Thursday" => "Четвер",
+"Friday" => "П'ятниця",
+"Saturday" => "Субота",
+"Sunday" => "Неділя"
+];
+foreach ($days as $english => $ukrainian) {
+echo "$english - $ukrainian<br>";
+}
