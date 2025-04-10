341193c75dd9cc846cf74250853d7b10535ecf32
David <kn1b22.tokovenko@kpnu.edu.ua>
2025-04-10 12:35:58 +0300

    .

--- LAB04/task1.php
+++ LAB04/task1.php
@@ -1,9 +1,11 @@
+<?php
 function factorial($n) {
-if ($n <= 1) {
-return 1;
-}
-return $n * factorial($n - 1);
-}
-for ($i = 1; $i <= 5; $i++) {
-echo "Факторіал $i: " . factorial($i) . "<br>";
-}
+    if ($n <= 1) {
+    return 1;
+    }
+    return $n * factorial($n - 1);
+    }
+    for ($i = 1; $i <= 5; $i++) {
+    echo "Факторіал $i: " . factorial($i) . "<br>";
+    }
+?>
